7d1032d357809b2a7307af4b451174a94df6d2bd
ushisantoasobu <babblemann.shunsee@gmail.com>
2016-01-07 01:54:35 +0900

    first impl

--- Example/Piojo/ViewController.swift
+++ Example/Piojo/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import Piojo
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +22,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func btnTapped(sender: AnyObject) {
+        self.label.pj_animate(self.textField)
     }
 
 }
---
+++ Pod/Classes/Piojo.swift
@@ -0,0 +1,14 @@
+extension UILabel {
+    public func pj_animate(to :String) {
+
+    }
+
+    public func pj_animate(to :String, duration :CGFloat) {
+
+    }
+
+    public func pj_animate(to :String, step :UInt) {
+
+    }
+}
+
